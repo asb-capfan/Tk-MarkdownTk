7d1ef6dd65b5e5a42eafbc8cbd55328e5fbb5f90
Jimi Carlo Wills <jimi@cpan.org>
2013-12-30 15:34:53 +0000

    Import of JIMI/Tk-MarkdownTk-0.02 from CPAN.
    gitpan-cpan-distribution: Tk-MarkdownTk
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         JIMI/Tk-MarkdownTk-0.02.tar.gz
    gitpan-cpan-author:       JIMI
    gitpan-cpan-maturity:     released

--- lib/Tk/MarkdownTk.pm
+++ lib/Tk/MarkdownTk.pm
@@ -10,14 +10,14 @@ Tk::MarkdownTk - a Tk::Markdown with tk widget tag support
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 use base qw(Tk::Derived Tk::Markdown);
-Construct Tk::Widget 'Markdown';
+Construct Tk::Widget 'MarkdownTk';
 
 
 
